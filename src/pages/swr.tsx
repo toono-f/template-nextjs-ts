60abb5238a3fc9ec405c0035bbaced8ddb3b09c7
toono <toono@markernet.co.jp>
2022-07-17 21:35:15 +0900

    swr実装

---
+++ src/pages/swr.tsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import type { NextPage } from "next";
+import useSWR from "swr";
+
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+};
+
+const fetcher = (url: string) => axios.get(url).then((res): Post => res.data);
+
+const Home: NextPage = () => {
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts/1",
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  return (
+    <div style={{ padding: "1rem" }}>
+      <p>{data?.id}</p>
+      <p>{data?.title}</p>
+      <p>{data?.body}</p>
+    </div>
+  );
+};
+
+export default Home;
